96505c31378187afa970cf06c570790750b8ab63
marwah20alshakhli <72710465+marwah20alshakhli@users.noreply.github.com>
2021-03-03 00:48:50 +0100

    added bootstrap, axios and weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
+import Axios from "axios";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Weather/>
+      <footer>
+        This is <a href="https://github.com/marwah20alshakhli/create-weather-app" target="_blank">
+        coded
+        </a> 
+        by Marwah
+      </footer>
     </div>
   );
 }
